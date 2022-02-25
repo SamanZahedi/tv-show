fd9527e7452f06d004316d0dbcb231f00993bb90
Saman <saman.zahedi10@gmail.com>
2022-02-25 00:42:12 +0000

    level 200 completed

--- script.js
+++ script.js
@@ -1,12 +1,53 @@
 //You can edit ALL of the code here
+const searchBox = document.getElementById("search-box");
+const searchCount = document.getElementById("search-count");
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
+  searchBox.addEventListener("keyup", onSearchKeyUp);
 }
 
-function makePageForEpisodes(episodeList) {
+const formatTitleNumbers = (number) =>
+  number < 10 ? `0${number}` : `${number}`;
+
+function makePageForEpisodes(allEpisodes) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  rootElem.innerHTML = "";
+  // rootElem.textContent = `Got ${allEpisodes.length} episode(s)`;
+  allEpisodes.forEach((episode) => {
+    let episodeContainer = document.createElement("div");
+    episodeContainer.className = "episode";
+    let title = document.createElement("h3");
+    title.innerText = `${episode.name} - S${formatTitleNumbers(
+      episode.season
+    )}E${formatTitleNumbers(episode.number)}`;
+    let episodeImage = document.createElement("img");
+    episodeImage.src = episode.image.medium;
+    let summaryOfEpisode = document.createElement("p");
+    summaryOfEpisode.innerText = episode.summary
+      .replace("<p>", "")
+      .replace("</p>", "");
+    episodeContainer.appendChild(title);
+    episodeContainer.appendChild(episodeImage);
+    episodeContainer.appendChild(summaryOfEpisode);
+    rootElem.appendChild(episodeContainer);
+  });
+}
+function onSearchKeyUp(event) {
+  const searchTerm = event.target.value.toLowerCase();
+  const allEpisodes = getAllEpisodes();
+  const filteredEpisodes = allEpisodes.filter((e) => {
+    const episodeName = e.name.toLowerCase();
+    const episodeSummary = e.summary.toLowerCase();
+    return (
+      episodeName.includes(searchTerm) || episodeSummary.includes(searchTerm)
+    );
+  });
+  const filteredCount = filteredEpisodes.length;
+  const allCount = allEpisodes.length;
+  const countString = `Displaying ${filteredCount} / ${allCount}`;
+  searchCount.innerText = countString;
+  makePageForEpisodes(filteredEpisodes);
 }
 
 window.onload = setup;
